f911c1918c1a1e51bad1604eaa6d7d6b503a0e0b
Vanya <i.boychyshyn@gmail.com>
2023-06-22 00:11:47 +0300

    commited

---
+++ main.js
@@ -0,0 +1,29 @@
+const result = 5 + 5 + '5';
+
+console.log("result :>>>", result)
+
+console.log("typeof result :>>>", typeof (result))
+
+const email = "afjafsjdf@gmail.dot";
+
+console.log("email includes @ :>>>", email.includes("@"))
+
+console.log("email length :>>>", email.length)
+
+const my = "My";
+
+const name = "name";
+
+const is = "is";
+
+const fullName = my + name + is + "Viktor";
+
+console.log("fullName :>>>", fullName)
+
+const userName = "Віктор";
+
+const payment = 1900;
+
+const message = `Дякуємо, ${userName}е! До сплати ${payment} гривень`;
+
+alert(message)
